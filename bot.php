b1828318454f78e31f584d6cb5a078a19ca53e2c
Rachit Preeyong <rachit_preeyong@hotmail.com>
2017-08-20 12:07:28 +0700

    Update bot.php verify.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 $proxy = "velodrome.usefixie.com:80";
-$proxyauth = 'fixie:cE0Sp7eA5dM9Jgi';
+$proxyauth = "fixie:cE0Sp7eA5dM9Jgi";
 $access_token = 'KRrnY2u6LoCMruxgWPt3EjnTUmgStpmhERTg4WBnh07GeEvfvjc76MtNdyS6bUvfKs5lnJ8P01NnYZ5OoJXW+leoaKO/Vd8ENTK8K1k957ekkFCyDjMQH8yz46eL/OlpJ6qEgc4ft/KNERkAo7lwVgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
@@ -39,12 +39,10 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            //curl_setopt($ch, CURLOPT_PROXY, $proxy);
-            //curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-
+            curl_setopt($ch, CURLOPT_PROXY, $proxy);
+            curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 			$result = curl_exec($ch);
 			curl_close($ch);
-
 			echo $result . "\r\n";
 		}
 	}
