d431317e988ba0689f5bdf9eec1c8449742c1dca
Rachit Preeyong <rachit_preeyong@hotmail.com>
2017-08-20 12:58:02 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -3,7 +3,9 @@ $proxy = "velodrome.usefixie.com:80";
 $proxyauth = "fixie:cE0Sp7eA5dM9Jgi";
 $access_token = 'KRrnY2u6LoCMruxgWPt3EjnTUmgStpmhERTg4WBnh07GeEvfvjc76MtNdyS6bUvfKs5lnJ8P01NnYZ5OoJXW+leoaKO/Vd8ENTK8K1k957ekkFCyDjMQH8yz46eL/OlpJ6qEgc4ft/KNERkAo7lwVgdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+//$url = 'https://api.line.me/v1/oauth/verify';
+
+$url = 'https://www.passinwallet.com';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
