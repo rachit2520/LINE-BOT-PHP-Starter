b1f3cf09b7d2d22229c380c84a6e67b773370c25
Rachit Preeyong <rachit_preeyong@hotmail.com>
2017-08-20 21:22:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,8 +1,8 @@
 <?php
 $proxy = "velodrome.usefixie.com:80";
 $proxyauth = "fixie:cE0Sp7eA5dM9Jgi";
-$access_token = 'KRrnY2u6LoCMruxgWPt3EjnTUmgStpmhERTg4WBnh07GeEvfvjc76MtNdyS6bUvfKs5lnJ8P01NnYZ5OoJXW+leoaKO/Vd8ENTK8K1k957ekkFCyDjMQH8yz46eL/OlpJ6qEgc4ft/KNERkAo7lwVgdB04t89/1O/w1cDnyilFU=';
-
+$Channel_Access_Token = 'KRrnY2u6LoCMruxgWPt3EjnTUmgStpmhERTg4WBnh07GeEvfvjc76MtNdyS6bUvfKs5lnJ8P01NnYZ5OoJXW+leoaKO/Vd8ENTK8K1k957ekkFCyDjMQH8yz46eL/OlpJ6qEgc4ft/KNERkAo7lwVgdB04t89/1O/w1cDnyilFU=';
+$Channel_Secret = '398582946851eea5be12139edf49577b';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -25,25 +25,33 @@ if (!is_null($events['events'])) {
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			// $url = 'https://api.line.me/v2/bot/message/reply';
+			// $data = [
+			// 	'replyToken' => $replyToken,
+			// 	'messages' => [$messages],
+			// ];
+			// $post = json_encode($data);
+			// $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_PROXY, $proxy);
-            curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-			echo $result . "\r\n";
+			// $ch = curl_init($url);
+            // curl_setopt($ch, CURLOPT_PROXY, $proxy);
+            // curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			// $result = curl_exec($ch);
+			// curl_close($ch);
+			// echo $result . "\r\n";
+
+            $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+            $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $Channel_Secret]);
+
+            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+
+            echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 		}
 	}
 }
